5d9ecb13545c4d4e8ae4bcc0c1b36267814694e0
Althea Panaligan <altheazpanaligan10@gmail.com>
2024-11-20 18:16:26 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+//id selector
+const content=document.querySelector("#content")
+
+//loading page
+window.addEventListener("load", () =>{
+    getUsers()
+})
+
+function getUsers(){
+    let html=""
+
+    fetch("https://bscsapicrud1.onrender.com/api/members", {mode: "cors"})
+    //fetch("http://localhost:5000/api/members", {mode: "cors"})
+    .then((response) =>{
+        console.log(response)
+        return response.json()
+    })
+    .then((data)=>{
+        console.log(data)
+        data.forEach((element) =>{
+            html += `<li>${element.first_name} ${element.last_name}</li>`
+        })
+
+        content.innerHTML = html
+    })
+    .catch((error) =>{
+        console.log(error)
+    })
+
+}
